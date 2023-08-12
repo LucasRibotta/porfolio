a318019154c0a11b8870ccd0320668e0b33ae99a
LucasRibotta <lucasribotta15@gmail.com>
2023-08-12 14:03:36 -0300

    responsive project render

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -5,7 +5,7 @@ import { projects } from '../../data'
 
 const Projects = () => {
   return (
-    <div className="pt-[3rem] mt-[4rem] mx-3 sm:mx-auto xl:mx-auto sm:text-center bg-white text-black rounded-[50px] shadow-lg shadow-slate-900" id="projects">
+    <div className="pt-[3rem] mt-[4rem] mx-3 sm:mx-auto xl:mx-auto 2xl:mx-3 sm:text-center bg-white text-black rounded-[50px] shadow-lg shadow-slate-900" id="projects">
       <div className='flex flex-col ml-2 '>
         <h2 className='text-violet-800 font-bold '>PORTFOLIO</h2>
         <p className='mt-8 font-medium '>Les presento los proyectos en los que participe.</p>
